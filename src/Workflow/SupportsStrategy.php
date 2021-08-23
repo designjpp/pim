8da7292b839c0f7bed982d818275d5e9945a3610
Eder Oliveira <ederoliveira@iMac-de-Eder-3.local>
2021-08-22 22:23:34 -0300

    Update SupportsStrategy.php

--- src/Workflow/SupportsStrategy.php
+++ src/Workflow/SupportsStrategy.php
@@ -15,7 +15,7 @@
 
 namespace App\Workflow;
 
-use App\Model\Product\Car;
+// use App\Model\Product\Car;
 use Symfony\Component\Workflow\SupportStrategy\WorkflowSupportStrategyInterface;
 use Symfony\Component\Workflow\WorkflowInterface;
 
@@ -23,12 +23,12 @@ class SupportsStrategy implements WorkflowSupportStrategyInterface
 {
     public function supports(WorkflowInterface $workflow, $subject): bool
     {
-        if ($workflow->getName() == 'product_data_enrichment') {
-            if ($subject instanceof Car && strpos($subject->getFullPath(), '/upload/new') === 0) {
-                return true;
-            }
-        }
+        // if ($workflow->getName() == 'product_data_enrichment') {
+        //     if ($subject instanceof Car && strpos($subject->getFullPath(), '/upload/new') === 0) {
+        //         return true;
+        //     }
+        // }
 
-        return false;
+        return [];
     }
 }
